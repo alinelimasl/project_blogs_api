630b3a88f6928ce3626c27fe98db93f372a67a85
Aline <aline_lima@outlook.com>
2023-12-14 09:50:48 -0300

    Requisito 2: cria model de User

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        displayName: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(255),
+            unique: true,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        },
+    }, {
+        tableName: 'users',
+        timestamps: false,
+        underscored: true,
+    });
+
+    return User;
+
+}
+
+module.exports = UserModel;

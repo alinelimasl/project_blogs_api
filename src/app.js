2d43ec5b308ab8a2df6b118a1d233128b16910eb
Aline <aline_lima@outlook.com>
2023-12-14 18:21:13 -0300

    Requisito 3: /login

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
+const jwt = require('jsonwebtoken'); 
 const express = require('express');
+const { User } = require('./models');
 
 // ...
 
@@ -11,8 +13,30 @@ app.get('/', (_request, response) => {
 
 app.use(express.json());
 
-// ...
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+ 
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Some required fields are missing' });
+  }
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
+  const user = await User.findOne({ where: { email } });
+
+  if (!user || user.password !== password) {
+    return res.status(400).json({ message: 'Invalid fields' });
+  }
+
+  const payload = {
+    id: user.id,
+    username: user.username,
+    admin: false,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+
+  res.status(200).json({ token });
+});
+
 module.exports = app;

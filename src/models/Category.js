81ad216af96647e42b28827ed1f6afa37e4e9852
Aline <aline_lima@outlook.com>
2023-12-15 15:26:57 -0300

    Requisito 7: cria category model

---
+++ src/models/Category.js
@@ -0,0 +1,23 @@
+const CategoryModel = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+    }, {
+        tableName: 'categories',
+        timestamps: false,
+        underscored: true,
+    });
+
+    return Category;
+
+}
+
+module.exports = CategoryModel;
